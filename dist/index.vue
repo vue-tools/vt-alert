ccd1fdf970b09a4289ab98216e2aa0e43f2065a3
2046 <g@ggg.gd>
2017-01-10 18:25:06 +0800

    init

---
+++ dist/index.vue
@@ -0,0 +1,32 @@
+<style src="./style.css"></style>
+<template src="./template.html"></template>
+
+<script>
+    import Vue from 'vue'
+    import plugin from './plugin'
+
+    let Component = {
+        props: {
+            visible: {
+                type: Boolean,
+                required: true
+            }
+        },
+        data() {
+            return {
+                access: false
+            }
+        },
+        methods: {
+            handle(time) {
+                setTimeout(() => {
+                    this.access = !this.access
+                }, time)
+            }
+        }
+    }
+
+    Vue.use(plugin(Component))
+
+    export default Component
+</script>
